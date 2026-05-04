9e7c4b68f90c24153485c6b03c1731e5f8c0a3d4
Veronicah Bironga <69029669+Ronnieb254@users.noreply.github.com>
2026-05-04 12:02:11 +0300

    yml

--- src/index.js
+++ src/index.js
@@ -10,11 +10,35 @@ const resolvers = require('./graphql/resolvers');
 const createContext = require('./graphql/context');
 const config = require('./config');
 
+const allowlist = [
+  "http://localhost:8080",
+  "http://localhost:8081",
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://thoughtcanva.com",
+]
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = {
+      origin: true,
+      credentials: true,
+      methods: "POST, GET ,PUT, OPTIONS, DELETE",
+    }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 async function startServer() {
   const app = express();
 
   // Middleware
-  app.use(cors());
+  app.use(cors(corsOptionsDelegate));
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ extended: true }));
 
