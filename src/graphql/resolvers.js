04e537410be883d71a87aad6f2f902511f7e504a
Veronicah Bironga <69029669+Ronnieb254@users.noreply.github.com>
2026-06-10 05:59:48 +0300

    issue

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -383,14 +383,12 @@ const resolvers = {
       });
     }
   },
-
-contactReplies: async (_, { contactId }, { prisma }) => {
-      return prisma.contactReply.findMany({
-        where: { contactId: parseInt(contactId) },
-        orderBy: { createdAt: "asc" },
-      });
-    },
-
+  contactReplies: async (_, { contactId }, { prisma }) => {
+  return prisma.contactReply.findMany({
+    where: { contactId: parseInt(contactId) },
+    orderBy: { createdAt: "asc" },
+  });
+},
 sendContactReply: async (_, { contactId, message }, { prisma }) => {
   const contact = await prisma.contact.findUnique({
     where: { id: parseInt(contactId) },
